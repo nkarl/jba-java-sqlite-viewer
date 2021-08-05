6d3e05a002567175e607ce4c70e45809dc26b908
nkarl <nordenk@pm.me>
2021-08-05 10:13:12 -0700

    finished stage-1

--- SQLite Viewer/task/src/viewer/ApplicationRunner.java
+++ SQLite Viewer/task/src/viewer/ApplicationRunner.java
@@ -4,5 +4,4 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         new SQLiteViewer();
     }
-
 }
--- SQLite Viewer/task/src/viewer/SQLiteViewer.java
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -5,10 +5,28 @@ import javax.swing.*;
 public class SQLiteViewer extends JFrame {
 
     public SQLiteViewer() {
+        super("SQLite Viewer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 900);
         setLayout(null);
         setResizable(false);
         setLocationRelativeTo(null);
+
+        initApp();
+        setVisible(true);
+    }
+
+    private void initApp() {
+        int textField_tail = 200;
+
+        JTextField textField = new JTextField();
+        textField.setName("FileNameTextField");
+        textField.setBounds(5, 20, getWidth() -textField_tail, 30);
+        add(textField);
+
+        JButton openButton = new JButton("Open");
+        openButton.setName("OpenFileButton");
+        openButton.setBounds(getWidth() -textField_tail + 15, 20, 100, 30);
+        add(openButton);
     }
 }
